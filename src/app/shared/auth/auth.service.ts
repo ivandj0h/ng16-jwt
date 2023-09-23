1822d3b7638136bc90d47336942e2493f46c0eda
ivandjoh <idsta.ivandi@xl.co.id>
2023-09-23 22:02:08 +0700

    service code updated

--- src/app/shared/auth/auth.service.ts
+++ src/app/shared/auth/auth.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
+import {LoginModel} from "../../auth/login-model";
+import {catchError, map, of} from "rxjs";
+import {TokenModel} from "./token-model";
 
 @Injectable({
   providedIn: 'root'
@@ -7,4 +10,26 @@ import {HttpClient} from "@angular/common/http";
 export class AuthService {
 
   constructor(private http:HttpClient) { }
+
+  userLogin(payload:LoginModel){
+    return this.http.post('http://localhost:3000/auth/login',payload)
+      .pipe(
+        map(
+          (data) => {
+            var tokens = data as TokenModel
+
+            // save tokens to local storage
+            localStorage.setItem('access_token',JSON.stringify(tokens))
+
+            return true;
+          }
+        ),
+        catchError(
+          (err) => {
+            console.log('error',err)
+            return of(false)
+          }
+        )
+      )
+  }
 }
